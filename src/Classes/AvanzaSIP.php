67c85c08677654d2741503b9185e1cbbf1141900
Alan Bertomeu <aculvi@gmail.com>
2025-07-15 12:22:14 +0200

    Rectificatives OK

--- src/Classes/AvanzaSIP.php
+++ src/Classes/AvanzaSIP.php
@@ -6,6 +6,7 @@ use AvanzaSip\Models\Empresa;
 use AvanzaSip\Enums\TipoRectificativa;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
 
 /**
  *
@@ -187,6 +188,10 @@ class AvanzaSIP
             if (isset($result->error_code)) {
                 throw new \Exception($result->error_message, $result->error_code);
             }
+            if(isset($response->success) && !$response->success){
+                Log::error(['response' => json_encode($response)]);
+                throw new \Exception('Error al enviar la factura a AvanzaSif: ' . $response->error_message,  $response->error_code);
+            }
             return $result;
         } catch (GuzzleException $e) {
             echo "\nError: {$e->getMessage()}\n";
@@ -201,7 +206,8 @@ class AvanzaSIP
      */
     public function altaFactura(Factura $factura)
     {
-        return $this->goCurl('altaFactura', $factura->toAlta());
+        $response = $this->goCurl('altaFactura', $factura->toAlta());
+        return $this->makeinvoiceResponse($response);
     }
 
     /**
@@ -212,6 +218,7 @@ class AvanzaSIP
     public function consultaFactura(Factura $factura):bool
     {
         return $this->goCurl('consultInvoice', $factura->toConsulta());
+
     }
 
     /**
@@ -221,7 +228,8 @@ class AvanzaSIP
      */
     public function editFactura(Factura $factura)
     {
-        return $this->goCurl('altaFactura', $factura->toEdit());
+        $response = $this->goCurl('altaFactura', $factura->toEdit());
+        return $this->makeinvoiceResponse($response);
     }
 
     /**
@@ -232,7 +240,8 @@ class AvanzaSIP
      */
     public function rectificativa(Factura $factura, Factura $rectificada)
     {
-        return $this->goCurl('altaFactura', $factura->toRectificativa($rectificada));
+        $response = $this->goCurl('altaFactura', $factura->toRectificativa($rectificada));
+        return $this->makeinvoiceResponse($response);
     }
 
     /**
@@ -263,5 +272,38 @@ class AvanzaSIP
     public function getQR(Factura $factura)
     {
         return $this->goCurl('getQR', $factura->toQR());
+    }
+
+    /**
+     * @param mixed $response
+     * @param $result
+     * @return array
+     */
+    protected function makeinvoiceResponse(mixed $response): array
+    {
+        $return = [
+            'success' => true,
+            'status' => $response['status'] ?? '',
+            'message' => 'Factura aceptada',
+            'response' => $response ?? null
+        ];
+        if (isset($response->success) && !$response->success) {
+            Log::error(['response' => json_encode($response)]);
+            $return['success'] = false;
+            $return['status'] = 'failed';
+            //$return['message'] = $result->error_message;
+        }
+        if (isset($response->status) && $response->status === 'Rechazada') {
+            Log::error(['response' => json_encode($response)]);
+            $return['success'] = false;
+            $return['message'] = "Verifactu a rechaazado la factura: {$response->error_message}";
+        }
+        if (isset($response['status']) && $response['status'] === 'Cola') {
+            $return['message'] = 'Factura en cola, el reponsable de la empresa ' . $this->invoicingCompany->name . ' recibirá un correo cuando se haya tramitado la factura';
+        }
+        if (isset($response['status']) && $response['status'] !== 'Rechazada' && $response['status'] !== 'Aceptada') {
+            $return['message'] = isset($response['error_message']) ? $response['error_message'] : 'Error desconocido';
+        }
+        return $return;
     }
 }
