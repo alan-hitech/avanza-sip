8d92b1ad11dad80c734ffb908987c579862de962
Alan Bertomeu <aculvi@gmail.com>
2025-07-17 10:53:51 +0200

    Rectificatives OK

--- src/Models/FacturaImpuesto.php
+++ src/Models/FacturaImpuesto.php
@@ -132,7 +132,7 @@ class FacturaImpuesto
     {
         $afactura = new \stdClass();
         $afactura->Impuesto = $this->getTipoImpuesto();
-        if(in_array($this->getImpuesto(), [Regimen::GENERAL, Regimen::BIENES_USADOS], true)) {
+        if(in_array($this->getImpuesto(), [TipoImpuesto::IVA,TipoImpuesto::IPSI], true)) {
             $afactura->ClaveRegimen = $this->getRegimen();
         }
         $afactura->CalificacionOperacion = $this->getCalificacionOperacion();
